36160ccfb58cfea22d6bdb5e2da674c387119243
Kevin Masters <irimiuke@gmail.com>
2024-07-12 20:19:30 -0400

    revise permissions for posts. simplify restriction method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,2 @@
 class ApplicationController < ActionController::Base
-
-  helper_method :current_user, :logged_in?
-
-  def logged_in?
-    !!current_user
-  end
-
-  def require_user
-    if !user_signed_in?
-      flash[:alert] = "you must be logged in to perform that action"
-      redirect_to root_path
-    end
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_08_234900) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_13_000129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_08_234900) do
     t.string "keywords"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
