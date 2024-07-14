01c26e95f4d3570ed3edf1d4bd488341faf2273b
Kevin Masters <irimiuke@gmail.com>
2024-07-14 16:00:38 -0400

    generate devise for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :users, only: [:show]
   get 'home/about'
   get 'posts/myposts'
