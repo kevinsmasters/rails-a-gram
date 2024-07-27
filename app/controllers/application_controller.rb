b12a33d74d58b9598e4ea3310363980593b21a36
Kevin Masters <irimiuke@gmail.com>
2024-07-26 20:08:54 -0400

    fix search troughout the rest of the site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
+
+  def set_search
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
+  end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,5 @@
 class ProfilesController < ApplicationController
   def index
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+
   end
-
-  # def users
-  #   query = User.ransack(username_cont: search_query)
-  #   @users = query.result(distinct: true)
-  # end
-
-  # def search_query
-  #   params[:query]
-  # end
 end
