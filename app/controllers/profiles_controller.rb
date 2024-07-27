2639a5aada03eb0f8a26ec74a0effc14587f23b0
Kevin Masters <irimiuke@gmail.com>
2024-07-26 20:00:02 -0400

    search working on profiles index

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,15 @@
 class ProfilesController < ApplicationController
   def index
-    @users = users
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
 
-  def users
-    query = User.ransack(username_cont: search_query)
-    @users = query.result(distince: true)
-  end
+  # def users
+  #   query = User.ransack(username_cont: search_query)
+  #   @users = query.result(distinct: true)
+  # end
 
-  def search_query
-    params[:query]
-  end
+  # def search_query
+  #   params[:query]
+  # end
 end
