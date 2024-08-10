a666685db83ab6a7fe0006eabc95950ccd3e8a3d
Kevin Masters <irimiuke@gmail.com>
2024-08-10 09:51:31 -0400

    move user id to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
@@ -74,6 +75,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :keywords, :user_id, images: [])
+      params.require(:post).permit(:title, :description, :keywords, images: [])
     end
 end
